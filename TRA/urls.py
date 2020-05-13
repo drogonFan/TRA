e3764de8357d4aabf2695f34bb3c4a6551eee22b
drogonFan <Drogon.Fan@gmail.com>
2020-05-13 21:06:23 +0800

    test

--- TRA/urls.py
+++ TRA/urls.py
@@ -20,5 +20,5 @@ from . import view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('homepage/', view.homepage),
-
+    path('overview/', view.overview),
 ]
--- TRA/view.py
+++ TRA/view.py
@@ -8,24 +8,5 @@ from rest_framework import routers, serializers, viewsets
 def homepage(request):
     return render(request, 'index.html')
 
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'is_staff']
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
-urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
+def overview(request):
+    return render(request, 'overview.html')
