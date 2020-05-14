973a39304c15499a18c605a9b43e5d2c2c140a8c
drogonFan <Drogon.Fan@gmail.com>
2020-05-14 15:30:01 +0800

    test

--- TRA/view.py
+++ TRA/view.py
@@ -7,6 +7,7 @@ from rest_framework import routers, serializers, viewsets
 from data.models import Record
 from django.views.decorators.csrf import csrf_exempt
 import json
+import datetime
 
 def homepage(request):
     return render(request, 'index.html')
@@ -24,7 +25,8 @@ def add_data_2017(request):
         # university = request.POST.get('university', default='')
         datas = json.loads(request.body.decode('utf-8'))
         print(datas)
-        record = Record(ttype=datas[0], pickup_datetime=datas[1], dropoff_datetime=datas[2], 
+        record = Record(ttype=datas[0], pickup_datetime=datetime.strptime(datas[1], '%Y-%m-%d %H:%M:%S'), 
+                dropoff_datetime=datetime.strptime(datas[2], '%Y-%m-%d %H:%M:%S'), 
                 passenger_count=datas[3], PULocationID=datas[4], DOLocationID=datas[5], 
                 payment_type=datas[6], trip_distance=datas[7],
                 fare_amount=datas[8], extra=datas[9],
